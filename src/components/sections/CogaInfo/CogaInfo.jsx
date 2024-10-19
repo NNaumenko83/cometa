c5ad6813887601630b34c0cfc7afa64665c7f280
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-10-19 21:33:21 +0300

    [Add] Images for CogaInfo

--- src/components/sections/CogaInfo/CogaInfo.jsx
+++ src/components/sections/CogaInfo/CogaInfo.jsx
@@ -1,9 +1,20 @@
 import { memo } from 'react'
 import Container from '../../Container/Container'
 import SectionTitle from '../../SectionTitle/SectionTitle'
-import { StyledCogaInfoSection, TitleTextWrapper, ContentWrapper } from './CogaInfo.styled'
+import {
+    StyledCogaInfoSection,
+    TitleTextWrapper,
+    ContentWrapper,
+    TriangleImg,
+    CapibaraImg,
+    ButtonWrapper,
+    CometaImg,
+} from './CogaInfo.styled'
 import InfoText from '../../Infotext/InfoText'
 import Button from '../../Button/Button'
+import Triangle from '../../../assets/images/CogaInfoTriangle.png'
+import CapibaraAustro from '../../../assets/images/CogaInfoCapibaraImg.png'
+import CometaImage from '../../../assets/images/CometaHero.png'
 
 const CogaInfo = () => {
     return (
@@ -13,14 +24,18 @@ const CogaInfo = () => {
                     <TitleTextWrapper>
                         <SectionTitle>what`s coga</SectionTitle>
                         <InfoText>
-                            $COGA, the native token of Comet Space, powers Poomb Creatures. Players earn, trade, and
-                            invest $COGA, creating real-world value. The Play-to-Earn (P2E) model lets players earn
-                            income through gameplay and NFTs.
+                            $COGA, the native token of Comet Space, powers Poomb Creatures. Players earn, trade, and invest $COGA, creating
+                            real-world value. The Play-to-Earn (P2E) model lets players earn income through gameplay and NFTs.
                         </InfoText>
                     </TitleTextWrapper>
-                    <Button>$COGA</Button>
+                    <ButtonWrapper>
+                        <CometaImg src={CometaImage} alt="Cometa" width={121} />
+                        <Button>$COGA</Button>
+                    </ButtonWrapper>
                 </ContentWrapper>
             </Container>
+            <TriangleImg src={Triangle} alt="Triangle" width={247} />
+            <CapibaraImg src={CapibaraAustro} alt="Triangle" width={248} />
         </StyledCogaInfoSection>
     )
 }
--- src/components/sections/CogaInfo/CogaInfo.styled.js
+++ src/components/sections/CogaInfo/CogaInfo.styled.js
@@ -1,16 +1,40 @@
 import styled from '@emotion/styled'
 
 export const StyledCogaInfoSection = styled.section`
-    padding-block-start: 7.5rem;
+    position: relative;
+    padding-block-start: 120px;
 `
 export const TitleTextWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 1.25rem;
+    gap: 20px;
 `
 export const ContentWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 18.9375rem;
+    gap: 303px;
     align-items: center;
+`
+
+export const TriangleImg = styled.img`
+    position: absolute;
+    top: 5.625rem;
+    right: 0;
+    z-index: -2;
+`
+export const CapibaraImg = styled.img`
+    position: absolute;
+    top: 16.25rem;
+    right: 0;
+    z-index: -1;
+`
+
+export const ButtonWrapper = styled.div`
+    position: relative;
+`
+
+export const CometaImg = styled.img`
+    position: absolute;
+    top: -100%;
+    left: -65%;
 `
