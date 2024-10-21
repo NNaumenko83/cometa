338e2ee0a4ab26cabf58af37f62a082fbfd381a3
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-10-21 09:22:34 +0300

    [Change] LogoHero

--- src/components/sections/Hero/Hero.jsx
+++ src/components/sections/Hero/Hero.jsx
@@ -1,14 +1,19 @@
 import { memo } from 'react'
-import { StyledHero, CometaImg } from './Hero.styled'
+import { StyledHero, CometaImg, CometaLogoImg, LogoWrapper } from './Hero.styled'
 import Container from '../../Container/Container'
 import CometaImage from '../../../assets/images/CometaHero.png'
 import HeroContentWrapper from './HeroContentWrapper/HeroContentWrapper'
+import CometaLogo from '../../../assets/images/HeroLogo.png'
 
 const Hero = () => {
     return (
         <StyledHero>
             <Container>
-                <CometaImg src={CometaImage} alt="Logo" width={175} />
+                <LogoWrapper>
+                    <CometaImg src={CometaImage} alt="Logo" width={500} />
+                    <CometaLogoImg src={CometaLogo} alt="Logo" width={707} />
+                </LogoWrapper>
+
                 <HeroContentWrapper />
             </Container>
         </StyledHero>
--- src/components/sections/Hero/Hero.styled.js
+++ src/components/sections/Hero/Hero.styled.js
@@ -1,16 +1,57 @@
 import styled from '@emotion/styled'
 import heroBackground from '../../../assets/images/Background_1.png'
+import heroBackgroundMobile from '../../../assets/images/Background_1_mobile.png'
 
 export const StyledHero = styled.section`
-    /* height: 100vh; */
     display: flex;
     width: 100%;
-    background-image: url(${heroBackground});
+    background-image: url(${heroBackgroundMobile});
     background-size: cover;
     background-position: 50% 15%;
     background-repeat: no-repeat;
+
+    @media screen and (min-width: 768px) {
+        background-image: url(${heroBackground});
+        background-position: 50% 0;
+    }
+`
+
+export const LogoWrapper = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    margin-block-start: 120px;
+
+    @media screen and (min-width: 768px) {
+        margin-block-start: 170px;
+    }
+    @media screen and (min-width: 1024px) {
+        margin-block-start: 260px;
+    }
 `
 
 export const CometaImg = styled.img`
-    margin: 90px auto 0;
+    width: 125px;
+    position: absolute;
+    top: -100%;
+    @media screen and (min-width: 768px) {
+        width: 300px;
+        top: -70%;
+    }
+    @media screen and (min-width: 1025px) {
+        width: 500px;
+        top: -55%;
+    }
+`
+
+export const CometaLogoImg = styled.img`
+    position: relative;
+    z-index: 2;
+    width: 150px;
+    @media screen and (min-width: 768px) {
+        width: 500px;
+    }
+    @media screen and (min-width: 1025px) {
+        width: 707px;
+    }
 `
